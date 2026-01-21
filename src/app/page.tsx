d179b4045c089cada0244816c07aec9ed5531b4e
Md Moniruzzaman <mdmoniruzzamanshuvo2003@gmail.com>
2026-01-21 09:52:07 +0600

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,8 +34,9 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-export default function Home() {
-  const token = cookies().get("authToken");
+export default async function Home() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("authToken");
 
   if (token) {
     redirect("/dashboard");
