5982bfb175347de4f43bafb18bde032a5a61a00f
Md Moniruzzaman <mdmoniruzzamanshuvo2003@gmail.com>
2026-01-21 07:22:47 +0600

    add button color and middle ware for auth

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
+  return NextResponse.next();
+
   let token = request.cookies.get("authToken")?.value;
   token = "demotokenvalue";
   const { pathname } = request.nextUrl;
